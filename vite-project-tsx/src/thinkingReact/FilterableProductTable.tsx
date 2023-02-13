443d99baae633e7c9d54fe2f5aa1607d33080711
PxViet <repsweet080303@gmail.com>
2023-02-13 16:54:48 +0700

    #12 completed example of Thinking in React

--- vite-project-tsx/src/thinkingReact/FilterableProductTable.tsx
+++ vite-project-tsx/src/thinkingReact/FilterableProductTable.tsx
@@ -22,6 +22,16 @@ class FilterableProductTable extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = { filterText: 'k', inStockOnly: true };
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+    this.handleInStockChange = this.handleInStockChange.bind(this);
+  }
+
+  handleFilterTextChange(filterText: string) {
+    this.setState({ filterText: filterText });
+  }
+
+  handleInStockChange(inStockOnly: boolean) {
+    this.setState({ inStockOnly: inStockOnly });
   }
   render(): React.ReactNode {
     return (
@@ -29,6 +39,8 @@ class FilterableProductTable extends React.Component<Props, State> {
         <SearchBar
           filterText={this.state.filterText}
           inStockOnly={this.state.inStockOnly}
+          onFilterTextChange={this.handleFilterTextChange}
+          onInStockChange={this.handleInStockChange}
         />
         <ProductTable
           product={this.props.product}
--- vite-project-tsx/src/thinkingReact/SearchBar.tsx
+++ vite-project-tsx/src/thinkingReact/SearchBar.tsx
@@ -3,18 +3,43 @@ import React from 'react';
 interface Props {
   filterText: string;
   inStockOnly: boolean;
+  onFilterTextChange: (value: string) => any;
+  onInStockChange: (value: boolean) => any;
 }
 
 class SearchBar extends React.Component<Props> {
+  constructor(props: Props) {
+    super(props);
+
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+    this.handleInStockChange = this.handleInStockChange.bind(this);
+  }
+  handleFilterTextChange(event: React.ChangeEvent<HTMLInputElement>) {
+    this.props.onFilterTextChange(event.target.value);
+  }
+
+  handleInStockChange(event: React.ChangeEvent<HTMLInputElement>) {
+    this.props.onInStockChange(event.target.checked);
+  }
+
   render(): React.ReactNode {
     const filterText = this.props.filterText;
     const inStockOnly = this.props.inStockOnly;
     return (
       <>
         <form>
-          <input type='text' placeholder='Enter search' value={filterText} />
+          <input
+            type='text'
+            placeholder='Enter search'
+            value={filterText}
+            onChange={this.handleFilterTextChange}
+          />
           <p className='description'>
-            <input type='checkbox' checked={inStockOnly} />
+            <input
+              type='checkbox'
+              checked={inStockOnly}
+              onChange={this.handleInStockChange}
+            />
             Only show products in stock
           </p>
         </form>
