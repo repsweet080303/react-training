675aee9c476ace752086ed6c180198fdc8727cdf
PxViet <repsweet080303@gmail.com>
2023-02-25 15:25:16 +0700

    #56 create ListItem storybook

---
+++ practice-one/src/components/common/ListItem/ListItem.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import ListItem, { IProps } from './index';
+
+export default {
+  title: 'PracticeOne/Common/ListItem',
+  component: ListItem,
+} as ComponentMeta<typeof ListItem>;
+
+const Template: ComponentStory<typeof ListItem> = function listItem({
+  href,
+  tagName,
+  title,
+}: IProps) {
+  return <ListItem href={href} tagName={tagName} title={title} />;
+};
+
+export const Primary = Template.bind({});
+Primary.args = {
+  href: '#',
+  tagName: 'a',
+  title: 'Home',
+};
