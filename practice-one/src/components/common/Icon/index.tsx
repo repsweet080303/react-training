b5fc8d6848f35de8aaf5c636fcb2632fd3823674
PxViet <repsweet080303@gmail.com>
2023-02-28 14:53:28 +0700

    #73 add props onClick for Icon, define icon Bars and dropdown menu in screen mobile, style List mobile

--- practice-one/src/components/common/Icon/index.tsx
+++ practice-one/src/components/common/Icon/index.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { IconProps } from '@/types/interfaces';
 
-const Icon = ({ icon, additionalClass }: IconProps) => (
-  <i className={`icon ${additionalClass}`}>
+const Icon = ({ icon, additionalClass, onClick }: IconProps) => (
+  <i
+    className={`icon ${additionalClass}`}
+    onClick={(e) => onClick?.(e)}
+    aria-hidden="true"
+  >
     <FontAwesomeIcon icon={icon} />
   </i>
 );
