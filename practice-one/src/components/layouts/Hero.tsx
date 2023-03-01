0d6658d9a7351a99caa7850d7c4e0de622d13747
PxViet <repsweet080303@gmail.com>
2023-03-01 16:53:31 +0700

    #81 create layout hero, fix style for list, typo, CardCourse

--- practice-one/src/components/layouts/Hero.tsx
+++ practice-one/src/components/layouts/Hero.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Typo from '../common/Typo';
+import Button from '@/components/common/Button';
 import CardCourse from '@/components/common/CardCourse';
 
 const Hero = () => (
@@ -34,6 +35,37 @@ const Hero = () => (
         >
           Every day brings with it a fresh set of learning possibilities.
         </Typo>
+        <div className="btn__wrapper">
+          <Button type="button" size="lg" variant="primary">
+            Get Quote Now
+          </Button>
+          <Button type="button" size="lg" variant="quaternary">
+            Learn More
+          </Button>
+        </div>
+      </div>
+      <div className="card-course__group">
+        <CardCourse
+          modify="expert"
+          title="Expert Instruction"
+          description="The gradual accumulation of
+        information about atomic and
+        small-scale behaviour..."
+        />
+        <CardCourse
+          modify="training"
+          title="Training Courses"
+          description="The gradual accumulation of
+        information about atomic and
+        small-scale behaviour..."
+        />
+        <CardCourse
+          modify="life"
+          title="Lifetime access"
+          description="The gradual accumulation of
+        information about atomic and
+        small-scale behaviour..."
+        />
       </div>
     </div>
   </main>
