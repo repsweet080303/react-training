4e7aa8d8a9a4abfe049905a1c6e0f2f0c5cec08b
PxViet <repsweet080303@gmail.com>
2023-03-03 10:48:41 +0700

    #98 create Card Product storybook

---
+++ practice-one/src/components/common/Card/Product/Product.stories.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import CardProduct from '@/components/common/Card/Product';
+import { ICardProduct } from '@/types/interfaces';
+
+export default {
+  title: 'PracticeOne/Common/CardProduct',
+  component: CardProduct,
+} as ComponentMeta<typeof CardProduct>;
+
+const Template: ComponentStory<typeof CardProduct> = ({
+  imageHeader,
+  tag,
+  alt,
+  title,
+  description,
+  downloaded,
+  vote,
+  newPrice,
+  oldPrice,
+}: ICardProduct) => (
+  <CardProduct
+    imageHeader={imageHeader}
+    tag={tag}
+    alt={alt}
+    title={title}
+    description={description}
+    downloaded={downloaded}
+    vote={vote}
+    newPrice={newPrice}
+    oldPrice={oldPrice}
+  />
+);
+
+export const TrainingProduct = Template.bind({});
+TrainingProduct.args = {
+  imageHeader: 'src/assets/images/keyboard-cover.png',
+  tag: 'Training Courses',
+  alt: 'keyboard cover',
+  title: 'Video in Live Action',
+  description:
+    "We focus on ergonomics and meeting you where you work. It's only a keystroke away.",
+  downloaded: 15,
+  vote: 4.5,
+  newPrice: 16.48,
+  oldPrice: 6.48,
+};
+
+export const BooksProduct = Template.bind({});
+BooksProduct.args = {
+  imageHeader: 'src/assets/images/pencil-cover.png',
+  tag: 'Training Courses',
+  alt: 'pencil cover',
+  title: 'book Library',
+  description:
+    "We focus on ergonomics and meeting you where you work. It's only a keystroke away.",
+  downloaded: 15,
+  vote: 4.5,
+  newPrice: 16.48,
+  oldPrice: 6.48,
+};
+
+export const CetifiedProduct = Template.bind({});
+CetifiedProduct.args = {
+  imageHeader: 'src/assets/images/color-pencil-cover.png',
+  tag: 'Get Quality Education',
+  alt: 'pencil color cover',
+  title: 'book Library',
+  description:
+    "We focus on ergonomics and meeting you where you work. It's only a keystroke away.",
+  downloaded: 15,
+  vote: 4.5,
+  newPrice: 16.48,
+  oldPrice: 6.48,
+};
