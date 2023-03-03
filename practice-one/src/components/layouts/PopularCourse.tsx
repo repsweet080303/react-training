8ef5f84ec2ca7ad6a3bd5718c9a03b14877d6dc9
PxViet <repsweet080303@gmail.com>
2023-03-03 15:18:50 +0700

    #99 create PopularCourse layout, import img size, add className for img, define container--large and spacing file label, list, listitem and typo

---
+++ practice-one/src/components/layouts/PopularCourse.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import Typo from '../common/Typo';
+import CardProduct from '@/components/common/Card/Product';
+
+const PopularCourse = () => (
+  <div className="popular-course">
+    <div className="container--large">
+      <div className="popular-course__content">
+        <Typo
+          statusTypo="typo__primary"
+          sizeTypo="sm"
+          tagTypo="h5"
+          weightTypo="typo__bold"
+          classTypo="typo__tag"
+        >
+          Practice Advice
+        </Typo>
+        <Typo
+          statusTypo="typo__secondary--light"
+          sizeTypo="xl"
+          tagTypo="h2"
+          weightTypo="typo__bold"
+          classTypo="typo__head"
+        >
+          Our Popular Courses
+        </Typo>
+        <Typo
+          statusTypo="typo__paragraph"
+          sizeTypo="sm"
+          tagTypo="p"
+          weightTypo="typo__regular"
+          classTypo="typo__sub--light"
+        >
+          Problems trying to resolve the conflict between
+          the two major realms of Classical physics: Newtonian mechanics
+        </Typo>
+      </div>
+      <div className="popular-course__card">
+        <CardProduct
+          imageHeader="src/assets/images/keyboard-cover.png"
+          alt="keyboard cover"
+          tag="Training Courses"
+          title="Video in Live Action"
+          description="We focus on ergonomics and meeting
+                     you where you work. It's only a
+                     keystroke away."
+          vote={4.5}
+          downloaded={15}
+          newPrice={6.48}
+          oldPrice={16.48}
+        />
+        <CardProduct
+          imageHeader="src/assets/images/pencil-cover.png"
+          alt="pencil cover"
+          tag="Books Liberary"
+          title="Every Client Matters"
+          description="We focus on ergonomics and meeting
+                     you where you work. It's only a
+                     keystroke away."
+          vote={4.5}
+          downloaded={15}
+          newPrice={6.48}
+          oldPrice={16.48}
+        />
+        <CardProduct
+          imageHeader="src/assets/images/color-pencil-cover.png"
+          alt="pencil color cover"
+          tag="Certified Teacher"
+          title="Get Quality Education"
+          description="We focus on ergonomics and meeting
+                     you where you work. It's only a
+                     keystroke away."
+          vote={4.5}
+          downloaded={15}
+          newPrice={6.48}
+          oldPrice={16.48}
+        />
+      </div>
+    </div>
+  </div>
+);
+
+export default PopularCourse;
--- practice-one/src/pages/HomePage.tsx
+++ practice-one/src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import Header from '@/components/layouts/Header';
 import Hero from '@/components/layouts/Hero';
 import Banner from '@/components/layouts/Banner';
 import Company from '@/components/layouts/Company';
-import CardProduct from '@/components/common/Card/Product';
+import PopularCourse from '@/components/layouts/PopularCourse';
 
 const HomePage = () => (
   <div className="cover">
@@ -11,19 +11,7 @@ const HomePage = () => (
     <Hero />
     <Banner />
     <Company />
-    <CardProduct
-      imageHeader="src/assets/images/keyboard-cover.png"
-      alt="keyboard cover"
-      tag="Training Courses"
-      title="Video in Live Action"
-      description="We focus on ergonomics and meeting
-                   you where you work. It's only a
-                   keystroke away."
-      vote={4.5}
-      downloaded={15}
-      newPrice={6.48}
-      oldPrice={16.48}
-    />
+    <PopularCourse />
   </div>
 );
 
