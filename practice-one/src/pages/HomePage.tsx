26c402387eff628eed3088a4716df53d76255301
PxViet <repsweet080303@gmail.com>
2023-03-06 16:00:31 +0700

    #116 create Feedback component and storybook

--- practice-one/src/pages/HomePage.tsx
+++ practice-one/src/pages/HomePage.tsx
@@ -5,6 +5,7 @@ import Banner from '@/components/layouts/Banner';
 import Company from '@/components/layouts/Company';
 import PopularCourse from '@/components/layouts/PopularCourse';
 import WatchPlan from '@/components/layouts/WatchPlan';
+import Feedback from '@/components/common/Feedback';
 
 const HomePage = () => (
   <div className="cover">
@@ -14,6 +15,18 @@ const HomePage = () => (
     <Company />
     <PopularCourse />
     <WatchPlan />
+    <Feedback
+      name="Regina Miles"
+      job="Designer"
+      comment="Slate helps you see
+      how many more days
+      you need to work to
+      reach your financial
+      goal for the month
+      and year."
+      url="src/assets/images/avatar-one.jpg"
+      alt="user one"
+    />
   </div>
 );
 
