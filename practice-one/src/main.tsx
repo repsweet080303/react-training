0d2872022ee2157ec94564bb970ae7bdcd28ef9e
PxViet <repsweet080303@gmail.com>
2023-03-10 11:41:10 +0700

    #159 divide section and comment folder pages and file App, main

--- practice-one/src/main.tsx
+++ practice-one/src/main.tsx
@@ -1,5 +1,8 @@
+// React
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// Component
 import App from '@/App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
